66004b2844dfe221924edcd9a3b5e480c4b69b51
Madhukar B <tejasbondre27@gmail.com>
2024-07-04 15:39:20 +0530

    added routing for todos page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleTodoRedirect = () => {
+    navigate('/Todo')
+  };
+
   return (
     <div>
+      <h2>Login</h2>
         <form action="">
             <div><label htmlFor="">Email: </label>
             <input type="text" placeholder='Enter youe email'/></div>
@@ -11,7 +19,7 @@ const Login = () => {
             <input type="password" placeholder='Enter your pass'/></div>
 
             <div>
-    <input type="submit" value='Login'/>
+    <input type="submit" value='Login' onClick={handleTodoRedirect}/>
     </div>
         </form>
     </div>
---
+++ src/Components/Todo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Todo = () => {
+    
+  return (
+    <div>
+        <h2>Your To-Do's</h2>
+    </div>
+  )
+}
+
+export default Todo
