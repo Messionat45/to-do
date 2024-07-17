dfd9e8007c8e0f6e0254ba0709c1716d5ac37ef5
Madhukar B <tejasbondre27@gmail.com>
2024-07-17 20:51:11 +0530

    ..

--- todoBackend/todoBackend/settings.py
+++ todoBackend/todoBackend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-pzdbat6@ek6$yr0svj5kw4jlysa3l(1tfh6m8n$qcq4#olvmqr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 # added .vercel.app for hosted procedure
 
 # Application definition
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles' #forhosting----
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
