1acee64943962e3945bb112c8dc656d8e1dd37f3
Madhukar B <tejasbondre27@gmail.com>
2024-07-19 15:13:19 +0530

    final commit

--- todoBackend/todoBackend/settings.py
+++ todoBackend/todoBackend/settings.py
@@ -28,12 +28,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'corsheaders',
+
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles' #forhosting----
+
 
 
 # Default primary key field type
@@ -132,8 +133,13 @@ CORS_ALLOWED_ORIGINS = [
     # 'http://localhost:3000/',  give a error if slash / is used at the end
 ]
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_RENDERER_CLASSES':('rest_framework.renderers.JSONRenderer',)
-# }  
-# for getting api in traditional way 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES':('rest_framework.renderers.JSONRenderer',), # for getting api in traditional way 
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    
+}  
+
 
--- todoBackend/todoBackend/urls.py
+++ todoBackend/todoBackend/urls.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 
+#simple jwt
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+
+    #simple jwt
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
 ]
+
