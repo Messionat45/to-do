a20564074d70847532ba45078b95f9b71387d48b
Madhukar B <tejasbondre27@gmail.com>
2024-07-22 15:33:16 +0530

    updated signup page

--- todoFrontend/src/Components/Signup.jsx
+++ todoFrontend/src/Components/Signup.jsx
@@ -1,29 +1,90 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const Signup = () => {
-    const navigate = useNavigate();
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const [signup, setSignup] = useState([]);
+   const navigate = useNavigate();
 
-    const handleLoginRedirect = () => {
-        navigate('/login');
+    
+
+    const handleName = (e) => {
+        setName(e.target.value);
+      }
+
+      const handleEmail = (e) => {
+        setEmail(e.target.value);
+      }
+
+      const handlePassword = (e) => {
+        setPassword(e.target.value);
+      }
+
+      const handleSignup = async (e) => {
+            e.preventDefault()
+
+            if(name && email && password){
+                const newUser = {
+                    name: name,
+                    email: email,
+                    password: password,
+                }
+
+                try {
+                    const insertSignup = await axios.post("http://127.0.0.1:8000/api/account/register/", newUser)
+                    setSignup([...signup,insertSignup.data])
+                    setName('');
+                    setEmail('');
+                    setPassword('');
+                    navigate('/login');
+                }
+                catch(error){
+                    console.log(error);
+                  }
+
+
+            }
+
     };
 
    return (
     <div>
         <h2>Sign Up</h2>
+
     <form action="">
-    <div><label htmlFor="">Name: </label>
-    <input type="text" placeholder='Enter your Name'/></div>
+    <div>
+        <label htmlFor="">Name: </label>
+        <input type="text" 
+        placeholder='Enter your Name'
+        value={name}
+        onChange={handleName}/>
+    </div>
 
-    <div><label htmlFor="">Email: </label>
-    <input type="text" placeholder='Enter your Email'/></div>
+    <div>
+        <label htmlFor="">Email: </label>
+        <input type="text"
+        placeholder='Enter your Email'
+        value={email}
+        onChange={handleEmail}/>
+    </div>
 
-    <div><label htmlFor="">Password: </label>
-    <input type="password" placeholder='Enter your Password'/></div>
+    <div>
+        <label htmlFor="">Password: </label>
+        <input type="password" 
+        placeholder='Enter your Password'
+        value={password}
+        onChange={handlePassword}/>
+    </div>
 
 
     <div>
-    <input type="submit" value='Signup' onClick={handleLoginRedirect}/>
+        <input type="submit" 
+        value='Signup' 
+        onClick={handleSignup}/>
     </div>
 
     </form>
